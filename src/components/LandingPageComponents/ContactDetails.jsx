ee45e52bcce2c53c06492a11e1d5d38e7872f46a
Emanuel Rigo <112441019+EmanuelRigo@users.noreply.github.com>
2025-01-13 23:06:14 -0300

    modifying ContactDetails.jsx

--- src/components/LandingPageComponents/ContactDetails.jsx
+++ src/components/LandingPageComponents/ContactDetails.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import Image from "next/image";
+import { FaWhatsapp, FaPhone, FaMapMarkerAlt, FaClock } from "react-icons/fa"; // Importar los íconos necesarios
 
 const ContactDetails = () => {
   return (
     <div className="p-6 bg-white rounded-lg">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-        {" "}
-        {/* Una columna en pantallas pequeñas, dos en medianas */}
         <div>
           <Image
             src="/image4.jpg" // Cambia esto por la ruta de tu imagen ficticia
@@ -29,11 +28,24 @@ const ContactDetails = () => {
             <h3 className="text-xl font-semibold text-sky-600">
               Sede Ficticia 1
             </h3>
-            <p className="mt-1 text-gray-600">Calle Ficticia 1234</p>
-            <p className="text-gray-600">Teléfono: 11 1234 5678</p>
-            <p className="text-gray-600">WhatsApp Directo: 11 9876 5432</p>
+            <p className="mt-1 text-gray-600 flex items-center">
+              <FaMapMarkerAlt className="mr-2 text-sky-600" />{" "}
+              {/* Icono de ubicación */}
+              Calle Ficticia 1234
+            </p>
+            <p className="text-gray-600 flex items-center">
+              <FaPhone className="mr-2 text-sky-600" />{" "}
+              {/* Icono de teléfono */}
+              Teléfono: 11 1234 5678
+            </p>
+            <p className="text-gray-600 flex items-center">
+              <FaWhatsapp className="mr-2 text-sky-600" />{" "}
+              {/* Icono de WhatsApp */}
+              WhatsApp Directo: 11 9876 5432
+            </p>
             <div className="mt-2">
-              <p className="font-semibold text-gray-700">
+              <p className="font-semibold text-gray-700 flex items-center">
+                <FaClock className="mr-2 text-sky-600" /> {/* Icono de reloj */}
                 Horario administrativo:
               </p>
               <p className="text-gray-600">
@@ -42,7 +54,8 @@ const ContactDetails = () => {
               <p className="text-gray-600">Sábados de 8:00 a 13:00hs.</p>
             </div>
             <div className="mt-2">
-              <p className="font-semibold text-gray-700">
+              <p className="font-semibold text-gray-700 flex items-center">
+                <FaClock className="mr-2 text-sky-600" /> {/* Icono de reloj */}
                 Horario de extracciones:
               </p>
               <p className="text-gray-600">
@@ -55,11 +68,24 @@ const ContactDetails = () => {
             <h3 className="text-xl font-semibold text-sky-600">
               Sede Ficticia 2
             </h3>
-            <p className="mt-1 text-gray-600">Avenida Ficticia 5678</p>
-            <p className="text-gray-600">Teléfono: 11 8765 4321</p>
-            <p className="text-gray-600">WhatsApp Directo: 11 6543 2109</p>
+            <p className="mt-1 text-gray-600 flex items-center">
+              <FaMapMarkerAlt className="mr-2 text-sky-600" />{" "}
+              {/* Icono de ubicación */}
+              Avenida Ficticia 5678
+            </p>
+            <p className="text-gray-600 flex items-center">
+              <FaPhone className="mr-2 text-sky-600" />{" "}
+              {/* Icono de teléfono */}
+              Teléfono: 11 8765 4321
+            </p>
+            <p className="text-gray-600 flex items-center">
+              <FaWhatsapp className="mr-2 text-sky-600" />{" "}
+              {/* Icono de WhatsApp */}
+              WhatsApp Directo: 11 6543 2109
+            </p>
             <div className="mt-2">
-              <p className="font-semibold text-gray-700">
+              <p className="font-semibold text-gray-700 flex items-center">
+                <FaClock className="mr-2 text-sky-600" /> {/* Icono de reloj */}
                 Horario administrativo:
               </p>
               <p className="text-gray-600">
@@ -68,7 +94,8 @@ const ContactDetails = () => {
               <p className="text-gray-600">Sábados de 9:00 a 14:00hs.</p>
             </div>
             <div className="mt-2">
-              <p className="font-semibold text-gray-700">
+              <p className="font-semibold text-gray-700 flex items-center">
+                <FaClock className="mr-2 text-sky-600" /> {/* Icono de reloj */}
                 Horario de extracciones:
               </p>
               <p className="text-gray-600">
